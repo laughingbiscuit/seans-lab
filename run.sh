3b46482cb1fc06c78513516a60b461da383d27fd
laughingbiscuit <sean@laughingbiscuit.com>
2021-11-01 11:53:17 +0000

    initial commit of simple kong

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker rm -f some-lbdk
 
 docker build -t lbdk .
-docker run --ulimit nofile=5000:5000 --net host --name some-lbdk -v $(pwd)/files:/root/files -v /var/run/docker.sock:/var/run/docker.sock -it lbdk sh
+docker run -e KONG_LICENSE --ulimit nofile=5000:5000 --net host --name some-lbdk -v $(pwd)/files:/root/files -v /var/run/docker.sock:/var/run/docker.sock -it lbdk sh
